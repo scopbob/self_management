684a1b089e7579c3199dd1f92a7532f4e6a82a07
scopbob <scopbob@gmail.com>
2024-03-13 14:12:29 +0900

    add category index

--- self_management/todo/views.py
+++ self_management/todo/views.py
@@ -40,7 +40,6 @@ class IndexView(LoginRequiredMixin, generic.ListView):
       return user_todo.filter(category__name=filter)
     return user_todo
 
-
   def get_context_data(self, **kwargs):
       context = super().get_context_data(**kwargs)
       context["category_list"] = Category.objects.filter(user=self.request.user)
